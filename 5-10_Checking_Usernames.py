89506e69ae31070927c8150db3f2c6ca213b15a8
KierkegaardRichIE <kierke7@outlook.com>
2019-08-02 17:22:41 +0800

    5-10 complete.

--- 5-10_Checking_Usernames.py
+++ 5-10_Checking_Usernames.py
@@ -1,1 +1,8 @@
-current
+current_users = ['Ada', 'Kierke', 'Donald', 'Eric', 'Dick']
+new_users = ['Kierke', 'Trump', 'White', 'Pinkman', 'Pokeman', 'dick']
+current_users_lower = [user.lower() for user in current_users]
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print('The ID: ' + new_user + ' is already in use, please enter a new ID!')
+    else:
+        print('You can use this ID, is it confirmed ?')
