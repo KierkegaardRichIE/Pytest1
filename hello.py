ae5b271c5c94f94fdc6370c0fc97bfefdf55522c
KierkegaardRichIE <kierke7@outlook.com>
2019-08-02 16:35:32 +0800

    5-8:Hello users!

--- hello.py
+++ hello.py
@@ -1,13 +1,9 @@
-age = 12
-if age < 2:
-    print('You are a little baby!')
-elif age >= 2 and age < 4:
-    print('You are a toddler!')
-elif age >= 4 and age < 14:
-    print('You are a child!')
-elif age >= 14 and age < 18:
-    print('You are a teenager!')
-elif age >= 18 and age < 65:
-    print('You are an adult!')
-elif age >= 65:
-    print('You are an elder!')
+users = ['Admin', 'Kierke', 'Ronaldo', 'vini bear', 'Ada', 'Trump']
+users.sort()
+for user in users:
+    if user == 'Admin':
+        print('Hello, ' + user + ' would you like to see a status report?')
+    if user == 'Trump':
+        print('The 93-year-old president of Zimbabwe was recently asked in a talk show: "If time passes, what do you want to do most?"Mugabe answered: "I want to go back to 1945, find Trump\'s father and give him a condom.')
+    if user != 'Admin' and user != 'Trump':
+        print('Hello,' + user + ' think you for you logging in!')
