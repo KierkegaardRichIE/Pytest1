b79ceb30b9d3fed98f303341c21f26b755b2162e
KierkegaardRichIE <kierke7@outlook.com>
2019-08-02 15:51:04 +0800

    Initial commit

---
+++ exc1.py
@@ -0,0 +1,9 @@
+name = ["qixueyuan", "qixuerui", "qxiuedong", "qixuetian"]
+message = "My little brother is " + name[0].title() + '.'
+print(message)
+name.append('qixuexin')
+name.append('qixueyao')
+name.insert(2,'broGiao')
+poped_name=name.pop()
+print(name)
+print(poped_name)
---
+++ hello.py
@@ -0,0 +1,13 @@
+age = 12
+if age < 2:
+    print('You are a little baby!')
+elif age >= 2 and age < 4:
+    print('You are a toddler!')
+elif age >= 4 and age < 14:
+    print('You are a child!')
+elif age >= 14 and age < 18:
+    print('You are a teenager!')
+elif age >= 18 and age < 65:
+    print('You are an adult!')
+elif age >= 65:
+    print('You are an elder!')
