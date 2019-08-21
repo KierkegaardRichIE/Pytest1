ebe7435214866f01bb5701503875177b55525d37
KierkegaardRichIE <kierke7@outlook.com>
2019-08-21 18:21:18 +0800

    Test nested dictionary in exc1.

--- chapter_6.py
+++ chapter_6.py
@@ -12,7 +12,43 @@ favorite_language = {
     'gates': 'c#',
     'linus': 'c++',
 }
+player_1 = {
+    'name': 'ronaldo',
+    'club': 'juventus',
+    'back_num': '7'
+}
+player_2 = {
+    'name': 'messi',
+    'club': 'barcalona',
+    'back_num': 10
+}
+player_3 = {
+    'name': 'bale',
+    'club': 'real madrid',
+    'back_num': 7
+}
+players_top3 = [player_1, player_2, player_3]
+for player in players_top3:
+    print(player)
+print('\n')
+river_list = {
+    'nile': 'egypt',
+    'Yellow-river': 'China',
+}
+
 players = []
+
+for player_number in range(11):
+    new_player = {
+        'speed': 15,
+        'shoot': 12,
+        'defend': 8
+    }
+    players.append(new_player)
+for player in players[:7]:
+    print(player)
+print('...')
+print('\n')
 
 print('kierke is from ' +
       kierkepz_ID['city'] +
@@ -28,10 +64,17 @@ for name, language in favorite_language.items():
     print(name.title() + "'s favorite language is " +
           language.title() +
           '.')
+print('\n')
 for name in favorite_language.keys():
     print(name.title() +
           ', thank you for your talking the poll.')
+print('\n')
+for language in favorite_language.values():
+    print(language.title())
+del favorite_language['mike']
+print('\n')
+
 for language in favorite_language.values():
     print(language.title())
 for player_number in range(11):
-    new_player={}
+    new_player = {}
--- exc1.py
+++ exc1.py
@@ -1,9 +1,20 @@
-name = ["qixueyuan", "qixuerui", "qxiuedong", "qixuetian"]
-message = "My little brother is " + name[0].title() + '.'
-print(message)
-name.append('qixuexin')
-name.append('qixueyao')
-name.insert(2,'broGiao')
-poped_name=name.pop()
-print(name)
-print(poped_name)
+players = {
+    'ronaldo': {
+        'first': 'Christiano',
+        'last': 'Ronaldo',
+        'location': 'Turin'
+    },
+    'messi': {
+        'first': 'Leo',
+        'last': 'Messi',
+        'location': 'Barcalona'
+    },
+}
+
+for player_name, player_info in players.items():
+    print("\nPlayer_name: " + player_name)
+    full_name = player_info['first'] + ' ' + player_info['last']
+    location = player_info['location']
+    
+    print("\tFull_name: " + full_name.title())
+    print("\tLocation: " + location.title())
